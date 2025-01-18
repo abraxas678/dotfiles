3cfaccfd89517f79f7a10b37b41577f1528b4c4c
380f19b0-171a-4cd5-a826-b24601628d1d <abraxas678@gmail.com>
2025-01-18 07:32:41 +0100

    Update tailscale.sh

--- run_once_tailscale.sh
+++ run_once_tailscale.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-/home/abrax/tmp/tailscale_setup.sh
+BACKEND=$(tailscale status -json | jq -r .BackendState)
+SELF=$(tailscale status -json | jq '.Self.Online')
+
+if [[ $BACKEND != "Running" ]] || [[ $SELF != "true" ]]; then
+  /home/abrax/tmp/tailscale_setup.sh
+fi
+
+
