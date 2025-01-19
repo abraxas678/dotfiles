08fa00f2afc451032e9536aa39ffc44e0039f1e8
380f19b0-171a-4cd5-a826-b24601628d1d <abraxas678@gmail.com>
2025-01-19 06:23:26 +0100

    Update bin/pueue_filtered.sh Remove non_home.sh Remove set_chown.sh

--- run_after_non_home.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo
-echo chez_non_home.sh
-echo; sleep 1
-/home/abrax/bin/chez_non_home.sh
-
--- run_before_set_chown.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo chown abrax: -R /home/abrax/.config/pcopy/
-sudo chown abrax: -R /home/abrax/.ssh
