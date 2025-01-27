9e779fffce6cd421d17d4ff47321bdc7b7441a86
380f19b0-171a-4cd5-a826-b24601628d1d <abraxas678@gmail.com>
2025-01-27 14:10:54 +0100

    Update big_files_check.sh Update install_pipx.sh

--- run_before_big_files_check.sh
+++ run_before_big_files_check.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-rclone lsf 
+#rclone lsf 
+chezmoi managed >/home/abrax/tmp/cz.managed
+while IFS= read -r line; do
+    #echo "$line"
+    dir=$(dirname "$line")
+    echo "$dir" >>/home/abrax/tmp/cz.managed.dirs
+done < /home/abrax/tmp/cz.managed
+cat /home/abrax/tmp/cz.managed.dirs | sort | uniq >/home/abrax/tmp/cz.managed.dirs.uniq
+#cat /home/abrax/tmp/cz.managed.dirs.uniq
+while IFS= read -r line; do
+    rich -p "/home/abrax/$line" -s blue 
+    bws run -- rclone lsf "/home/abrax/$line" >>/home/abrax/tmp/cz.managed.dirs.files
+    #while IFS= read -r file; do
+    #    rich -p "$file" -s green
+   #     size=$(stat -f %z "/home/abrax/$line/$file")
+   #     bws run -- rclone size --json "$file" | jq .bytes
+   # done < /home/abrax/tmp/cz.managed.dirs.files
+done < /home/abrax/tmp/cz.managed.dirs.uniq
+cat /home/abrax/tmp/cz.managed.dirs.files
