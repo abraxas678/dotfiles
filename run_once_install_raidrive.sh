d54db3b3bfc6f20d0597f531eb8f2c141d61a3b2
380f19b0-171a-4cd5-a826-b24601628d1d <abraxas678@gmail.com>
2025-02-04 16:59:19 +0100

    1

--- run_once_install_raidrive.sh
+++ run_once_install_raidrive.sh
@@ -3,5 +3,5 @@ mkdir -p $HOME/tmp
 cd $HOME/tmp
 wget https://app.raidrive.com/deb/pool/main/r/raidrive/raidrive_2024.9.27.6-linux_amd64.deb
 sudo apt update
-sudo apt install -y raidrive_2024.9.27.6-linux_amd64.deb
+sudo apt install -y ./raidrive_2024.9.27.6-linux_amd64.deb
 
