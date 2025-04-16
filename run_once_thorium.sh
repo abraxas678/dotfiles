c2af3fde64e65676a9f0d4544e2dd86e55536a77
380f19b0-171a-4cd5-a826-b24601628d1d <9d393c6c-b12c-4be5-a62c-b226006c7185>
2025-04-16 21:46:11 +0200

    Update bin/aa.sh.prompt.db Update bin/aa.sh.prompt_response.db Update bin/bookmarks.db Update bin/pueue_done.sh Update bin/setup_ansible_apps.yml Add .config/espanso/match/base.yml Remove .config/espanso/match/base.yml Remove .config/pueue/pueue.yml Add .config/pueue/pueue.yml Remove .config/rclone/rclone.conf Add .config/rclone/rclone.conf Remove .config/systemd/user/homebrew.pueue.service Add .config/systemd/user/homebrew.pueue.service Update .bashrc Update .ssh/known_hosts Add thorium.sh

---
+++ run_once_thorium.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+if [[ $(hostname) = *"nuc"* ]]; then
+[[ $(whoami) = "root" ]] && MYSUDO="" || MYSUDO="sudo"
+which thorium-browser >/dev/null 2>&1
+if [[ $? != 0 ]]; then
+    mkdir -p $HOME/tmp
+    cd $HOME/tmp
+    echo "install thorium browser"
+    $HOME/tmp/public/github_latest_release_url.sh Alex313031 thorium >url
+    URL=$(cat url | tail -n1)
+    echo; echo "URL $URL"; echo
+    echo; echo "wget $URL"; echo
+    wget $URL
+#    $MYSUDO apt install -y $HOME/tmp/$(basename $URL)
+    sudo apt install -y $HOME/tmp/$(basename $URL)
+    rm $(basename $URL)
+fi
+rm url -f
+fi
