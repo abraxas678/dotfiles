24b1da7a8a102ea42bb36668ae3533f5c95c6286
380f19b0-171a-4cd5-a826-b24601628d1d <9d393c6c-b12c-4be5-a62c-b226006c7185>
2025-04-19 10:27:57 +0200

    1

--- run_once_install_fagram.sh
+++ run_once_install_fagram.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ $(hostname) = *"nuc"* ]]; then
-URL=$(github_latest_release_url.sh FajoX1 fagramdesktop)
+URL=$($HOME/tmp/public/github_latest_release_url.sh FajoX1 fagramdesktop | tail -n 1)
 wget $URL
 sudo apt update
-sudo apt install ./$(basename $URL)
+sudo apt install ./$(basename $URL) -y
 fi
