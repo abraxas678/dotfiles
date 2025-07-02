2b63406933d926652f8ddf55950d578d6120fb32
abraxas678 <abraxas678@gmail.com>
2025-07-02 07:08:19 +0200

    Add bin/rich.sh Update install_fagram.sh

--- run_once_install_fagram.sh
+++ run_once_install_fagram.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 if [[ $(hostname) = *"nuc"* ]]; then
+if [[ ! -f /usr/bin/fagram ]]; then
 URL=$($HOME/tmp/public/github_latest_release_url.sh FajoX1 fagramdesktop | tail -n 1)
 wget $URL
 sudo apt update
 sudo apt install ./$(basename $URL) -y
+fi
 fi
